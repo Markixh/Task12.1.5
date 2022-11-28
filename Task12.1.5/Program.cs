557dee38ff1482a3360a604eaf1011b7c16b8b26
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-11-28 20:39:08 +0300

    Добавлен шаблон задания

--- Task12.1.5/Program.cs
+++ Task12.1.5/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Task12._1._5
+﻿namespace Task12_1_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        class User
+        {
+            public string Login { get; set; }
+            public string Name { get; set; }
+            public bool IsPremium { get; set; }
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
         }
     }
 }
